459bfab1a32548418a27f41f416eb1d168670e89
Faisal Khan <faisalkhan91@outlook.com>
2025-09-05 20:36:27 -0700

    test: Update app title in unit tests to reflect new project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FluxusUI'`, () => {
+  it(`should have as title 'Fluxus'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FluxusUI');
+    expect(app.title).toEqual('Fluxus');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FluxusUI app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Fluxus app is running!');
   });
 });
